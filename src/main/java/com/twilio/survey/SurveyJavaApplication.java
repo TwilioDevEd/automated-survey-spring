37cedac0bf17d48a36a9ea43104fd16bc49fc565
Mario Celi <mceli@stackbuilders.com>
2015-08-07 14:23:47 -0500

    Parse a survey from a text file

--- src/main/java/com/twilio/survey/SurveyJavaApplication.java
+++ src/main/java/com/twilio/survey/SurveyJavaApplication.java
@@ -1,13 +1,36 @@
 package com.twilio.survey;
 
+import com.twilio.survey.models.Survey;
+import com.twilio.survey.repositories.QuestionRepository;
+import com.twilio.survey.repositories.SurveyRepository;
+import com.twilio.survey.services.QuestionService;
+import com.twilio.survey.services.SurveyService;
+import com.twilio.survey.util.SurveyParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class SurveyJavaApplication {
+public class SurveyJavaApplication implements CommandLineRunner {
+  @Autowired
+  private QuestionRepository questionRepository;
+  @Autowired
+  private SurveyRepository surveyRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(SurveyJavaApplication.class, args);
+  }
+
+  @Override
+  public void run(String... strings) throws Exception {
+    SurveyService surveyService = new SurveyService(surveyRepository);
+    QuestionService questionService = new QuestionService(questionRepository);
+
+    SurveyParser surveyParser = new SurveyParser(surveyService, questionService);
+    surveyParser.parse("survey.json");
   }
 }
--- src/main/resources/db/migration/V1_2__Create_questions_table.sql
+++ src/main/resources/db/migration/V1_2__Create_questions_table.sql
@@ -2,6 +2,6 @@ create table questions (
     id SERIAL UNIQUE not null PRIMARY KEY,
     body varchar(255) not null,
     type varchar(30) not null,
-    survey_id int REFERENCES surveys (id),
+    survey_id int REFERENCES surveys (id) ON DELETE CASCADE,
     date timestamp not null
 );
