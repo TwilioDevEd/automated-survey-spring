c8ccd651fe0dae39ee5cfc20d317b918ec2ed010
Victor Shyba <victor.shyba@gmail.com>
2016-03-25 12:01:31 -0300

    Remove duplicated code on VoiceQuestionHandler
    VoiceQuestionHandler can only record, gather or hangup. This insight
    makes it possible to remove duplicated code between those operations.

--- src/main/java/com/twilio/survey/util/VoiceQuestionHandler.java
+++ src/main/java/com/twilio/survey/util/VoiceQuestionHandler.java
@@ -9,6 +9,15 @@ import com.twilio.survey.models.Question;
  */
 public class VoiceQuestionHandler implements QuestionHandler{
   private Question question;
+  private String recordingInstructions =
+          "Record your answer after the beep.";
+  String numericInstructions =
+          "For the next question select a number with the dial pad and then press the pound key";
+  String booleanInstructions =
+          "For the next question, press 1 for yes, and 0 for no. Then press the pound key.";
+  private String errorMessage =
+          "We are sorry, there are no more questions available for this survey. Good bye.";
+
 
   public VoiceQuestionHandler(Question question) {
     this.question = question;
@@ -19,76 +28,48 @@ public class VoiceQuestionHandler implements QuestionHandler{
    * the specific TwiMLResponse
    */
   public String getTwilioResponse() throws TwiMLException{
-    TwiMLResponse response = new TwiMLResponse();
     switch (question.getType()) {
       case "text":
-        return getTextResponse(response);
+        return getRecordTwiML();
       case "numeric":
-        return getNumericResponse(response);
+        return getGatherResponse(numericInstructions);
       case "yes-no":
-        return getYesNoResponse(response);
+        return getGatherResponse(booleanInstructions);
       default:
-        return response.toEscapedXML();
+        return getHangupResponse();
     }
   }
-
 
   /**
    * method that returns a generic TwiMLResponse when an non existent question is requested
    */
   public String getHangupResponse() throws TwiMLException{
-    String errorMessage =
-            "We are sorry, there are no more questions available for this survey. Good bye.";
     TwiMLResponse response =  new TwiMLResponse();
     response.append(new Say(errorMessage));
     response.append(new Hangup());
     return response.toEscapedXML();
   }
 
-  private String getTextResponse(TwiMLResponse response) throws TwiMLException {
-    String defaultMessage =
-        "Record your answer after the beep.";
-    Say say = new Say(defaultMessage);
-    Say questionSay = new Say(question.getBody());
-    Pause pause = new Pause();
+  private String getRecordTwiML() throws TwiMLException {
+    TwiMLResponse response = new TwiMLResponse();
+
+    response.append(new Say(recordingInstructions));
+    response.append(new Pause());
+    response.append(new Say(question.getBody()));
+
     Record record = new Record();
     record.setAction("/save_response?qid=" + question.getId());
     record.setMethod("POST");
     record.setTranscribe(true);
     record.setTranscribeCallback("/save_response?qid=" + question.getId());
     record.setMaxLength(60);
-
-    response.append(say);
-    response.append(pause);
-    response.append(questionSay);
     response.append(record);
 
     return response.toEscapedXML();
   }
 
-  private String getNumericResponse(TwiMLResponse response) throws TwiMLException {
-    String defaultMessage =
-        "For the next question select a number with the dial pad and then press the pound key";
-    Say say = new Say(defaultMessage);
-    Say questionSay = new Say(question.getBody());
-    Pause pause = new Pause();
-    Gather gather = new Gather();
-    gather.setAction("/save_response?qid=" + question.getId());
-    gather.setMethod("POST");
-    gather.setFinishOnKey("#");
-
-
-    response.append(say);
-    response.append(pause);
-    response.append(questionSay);
-    response.append(gather);
-
-    return response.toEscapedXML();
-  }
-
-  private String getYesNoResponse(TwiMLResponse response) throws TwiMLException {
-    String defaultMessage =
-            "For the next question, press 1 for yes, and 0 for no. Then press the pound key.";
+  private String getGatherResponse(String defaultMessage) throws TwiMLException {
+    TwiMLResponse response = new TwiMLResponse();
 
     response.append(new Say(defaultMessage));
     response.append(new Pause());
